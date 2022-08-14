e925d4e9ce5ceef5711371a6d5b27d39bb2a9e3b
Dishuk <kainhurst@gmail.com>
2022-08-14 12:12:32 +0300

    Fixed public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-    publicPath: '/GLTF-Viewer/docs/',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/GLTF-Viewer/'
+        : '/',
     assetsDir: './',
     outputDir: './docs',
     transpileDependencies: true,
