ff162c83fdbc9c94e91d0c65bc86fe81f73ba63c
Dishuk <kainhurst@gmail.com>
2022-08-14 11:40:06 +0300

    Changed build directory

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
     publicPath: '/',
     assetsDir: './',
+    outputDir: './docs',
     transpileDependencies: true,
 })
